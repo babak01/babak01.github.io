abf7d7e21663f8a0d4db6fc4224c465219e86e7a
Babak V-Ghaffari <54375769+babak01@users.noreply.github.com>
2023-08-02 23:12:15 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,34 +1,42 @@
-let selectedFile;
+import { fetch } from 'wix-fetch';
 
-document.getElementById("fileInput").addEventListener("change", (event) => {
-  const file = event.target.files[0];
+$w.onReady(function () {
+  $w("#fileInput").onChange((event) => {
+    const file = event.target.files[0];
+    console.log("File selected:", file.name); // Log the selected file name
 
-  if (!file.name.endsWith(".npy")) {
-    console.log("Please upload a .npy file.");
-    selectedFile = null;
-    return;
-  }
+    if (!file.name.endsWith(".npy")) {
+      console.log("Please upload a .npy file.");
+      selectedFile = null;
+      return;
+    }
 
-  selectedFile = file;
-});
+    selectedFile = file;
+  });
 
-document.getElementById("submitButton").addEventListener("click", () => {
-  if (!selectedFile) {
-    console.log("Please upload a .npy file.");
-    return;
-  }
+  $w("#submitButton").onClick((event) => {
+    if (!selectedFile) {
+      console.log("Please upload a .npy file.");
+      return;
+    }
 
-  let formData = new FormData();
-  formData.append('file', selectedFile);
+    console.log("Submitting the file:", selectedFile.name); // Log the submitted file name
 
-  fetch('http://172.31.83.137:5000/predict', {
-    method: 'POST',
-    body: formData
-  })
-  .then(response => response.json())
-  .then(data => {
-    document.getElementById("predictionText").textContent = 'Diagnosis: ' + data.diagnosis;
-    document.getElementById("resultImage").src = 'data:image/jpeg;base64,' + data.image;
-  })
-  .catch(error => console.error('Error:', error));
+    let formData = new FormData();
+    formData.append('file', selectedFile);
+
+    return fetch('http://172.31.83.137:5000/predict', {
+      method: 'POST',
+      body: formData
+    })
+    .then(response => response.json())
+    .then(data => {
+      $w("#predictionText").text = 'Diagnosis: ' + data.diagnosis;
+      $w("#resultImage").src = 'data:image/jpeg;base64,' + data.image;
+    })
+    .catch(error => {
+      console.error('Error:', error);
+      alert("An error occurred while submitting the file. Please check the console for details.");
+    });
+  });
 });
